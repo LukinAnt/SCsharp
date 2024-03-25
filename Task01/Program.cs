d5f7088451b5ef71e87bcc4216478daac131a476
Lukin Lukina <a.lukin@rambler.ru>
2024-03-25 18:27:32 +0300

    task03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,4 +1,21 @@
 ﻿/*
+напишите программу, которая на вход принимает число и выдает его квадрат 
+(число умноженное на само себя)
+4-16
+-3-9
+-7-49
+*/
+
+/*
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number * number;
+Console.WriteLine(result);
+*/
+
+
+/*
 Напишите программу, которая на вход принимает два числа и проверяет, 
 является ли первое число квадратом второго
 
@@ -8,7 +25,7 @@ a = 9, b = -3 => да
 a = -3, b = 9 => нет
 */
 
-Console.WriteLine("Введите два числа: ");
+/*Console.WriteLine("Введите два числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 
@@ -20,3 +37,54 @@ else
 {
     Console.WriteLine("нет");
 }
+*/
+
+/*
+Task03
+Напишите программу, которая будет выдавать название жгя недели по заданному номеру
+
+3 -> среда
+5 -> пятница
+я- почемуто на семинаре пишут else if. а у меня и так работает
+*/
+/*
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number ==1)
+{
+    Console.WriteLine("понедельник");
+}
+if (number ==2)
+{
+    Console.WriteLine("вторник");
+}
+if (number ==3)
+{
+    Console.WriteLine("среда");
+}
+if (number ==4)
+{
+    Console.WriteLine("четверг");
+}
+if (number ==5)
+{
+    Console.WriteLine("пятница");
+}
+if (number ==6)
+{
+    Console.WriteLine("суббота");
+}
+if (number ==7)
+{
+    Console.WriteLine("воскресенье");
+}
+if (number > 7)
+{
+    Console.WriteLine("нет такого дня");
+}
+*/
+
+/*
+
+*/
