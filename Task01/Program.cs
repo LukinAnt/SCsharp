f4e57df6bd3a22175fff324ec4f852e53c7e9036
Lukin Lukina <a.lukin@rambler.ru>
2024-03-25 22:45:49 +0300

    первый чеминар

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -112,7 +112,7 @@ Console.Write(i+" " );
 918 => 17
 */
 
-
+/*
 Console.WriteLine("Введите техзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if(number > 99 && number < 1000)
@@ -121,3 +121,92 @@ if(number > 99 && number < 1000)
 }
 else
 Console.WriteLine("число не трехзначное");
+*/
+/*
+2. Напишите программу, которая на вход принимает два числа 
+и выдает какое число больше а какое меньше
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+/*
+Console.WriteLine("Введите два число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine($"больше первое число {number1}");
+    Console.WriteLine($"меньше второе число {number2}");
+}
+else
+{
+    Console.WriteLine($"больше Второе число {number2}");
+    Console.WriteLine($"меньше первое число {number1}");
+
+}
+*/
+
+
+/*
+4. Напишите программу, которая  на вход принимает три числа и выдает 
+максимальное из этих чисел
+*/
+/*
+Console.WriteLine("Введите три числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if(number2 > max)
+{
+    max = number2;
+}
+if(number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine(max);
+*/
+
+
+/*
+6. Напишите программу, которая  на вход принимает число и выдает, 
+является ли число четным(желится ли на 2 без остатка)
+ 4
+ -3
+ 7
+*/
+/*
+Console.WriteLine("Введите числo: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number % 2;
+if(number2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нетт");
+}
+*/
+
+
+/*
+8. Напишите программу, которая на вход принимает число (N) и 
+на выходе показывает все четные числа от 1 до N
+
+ 5->2 4
+ 8->2 4 6 8
+*/
+
+/*
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= number; i+=2)
+{
+Console.Write($"{i} ");
+}
+*/
